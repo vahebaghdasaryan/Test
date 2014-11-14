d852e61f33ddea2d30d3620ddeaaefa229020d8f
vahebaghdasaryan-scdm <vahe.baghdasaryan@scdm.de>
2014-11-14 15:52:52 +0400

    First step to push some logic in GitHub

---
+++ TestArtifact/src/main/java/goup1/TestArtifact/App.java
@@ -0,0 +1,13 @@
+package goup1.TestArtifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Changed" );
+    }
+}
