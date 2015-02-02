1dc55bbc693d7a3e9ab5c42744654a6aa9c04f6d
vahebaghdasaryan-scdm <vahe.baghdasaryan@scdm.de>
2015-02-02 11:10:17 +0400

    Commit after project renaming

--- newTestArtifact/src/main/java/goup1/TestArtifact/App.java
+++ newTestArtifact/src/main/java/goup1/TestArtifact/App.java
@@ -2,6 +2,6 @@ package goup1.TestArtifact;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("This is new with new project name");
+		System.out.println("This is new with new project name-x");
 	}
 }
