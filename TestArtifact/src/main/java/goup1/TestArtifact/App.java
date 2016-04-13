c38f738060c4ae7114aa7900ad10f3cd78d891ac
vahebaghdasaryan <vahe.baghdasaryan>
2016-04-13 18:18:01 +0400

    add "try"

--- TestArtifact/src/main/java/goup1/TestArtifact/App.java
+++ TestArtifact/src/main/java/goup1/TestArtifact/App.java
@@ -3,6 +3,6 @@ package goup1.TestArtifact;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("This is new with new project name"
-				+ " change in the project cloned before project renaming");
+				+ " change in the project cloned before project renaming, try");
 	}
 }
