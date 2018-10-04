da12cb8472feabe24e8e23ef79182b638faa1a13
vahebaghdasaryan <vahe.baghdasaryan@gmail.com>
2018-10-04 11:39:36 +0400

    Add otherName in class B.

--- TestArtifact/src/main/java/goup1/TestArtifact/B.java
+++ TestArtifact/src/main/java/goup1/TestArtifact/B.java
@@ -5,6 +5,7 @@ package goup1.TestArtifact;
  */
 public class B {
     private String name;
+    private String otherName;
 
     public String getName() {
         return name;
@@ -12,5 +13,13 @@ public class B {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getOtherName() {
+        return otherName;
+    }
+
+    public void setOtherName(String otherName) {
+        this.otherName = otherName;
     }
 }
