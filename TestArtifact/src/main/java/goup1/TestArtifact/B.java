eed785119b701923318fdbe37513dd3062921efa
vahebaghdasaryan <vahe.baghdasaryan@gmail.com>
2018-10-04 11:38:36 +0400

    Add class B.

---
+++ TestArtifact/src/main/java/goup1/TestArtifact/B.java
@@ -0,0 +1,16 @@
+package goup1.TestArtifact;
+
+/**
+ * Created by user on 10/4/2018.
+ */
+public class B {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
