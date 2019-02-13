4a468aedba82919a38339f0569e6d56cd7622131
vahebaghdasaryan <vahe.baghdasaryan@gmail.com>
2019-02-13 11:41:22 +0400

    add text in some_release_1 branch.

--- TestArtifact/src/main/java/goup1/TestArtifact/App.java
+++ TestArtifact/src/main/java/goup1/TestArtifact/App.java
@@ -3,6 +3,6 @@ package goup1.TestArtifact;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("This is new with new project name"
-				+ " change in the project cloned before project renaming, test-x");
+				+ " change in the project cloned before project renaming, test-x in some_release_1 branch");
 	}
 }
